35c1a2f55abd01a32eec3d5f8a23ecce81bf8d31
Vojta Stavik <stavik@outlook.com>
2020-01-04 11:42:46 +0100

    [UIKit] Xcode project init

---
+++ WeatherApp-UIKit/WeatherApp-UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp-UIKit
+//
+//  Created by Vojta on 04/01/2020.
+//  Copyright © 2020 Industrial Binaries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
