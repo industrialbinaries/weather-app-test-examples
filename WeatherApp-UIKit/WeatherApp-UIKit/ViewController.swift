6de08d4f7198c87cacebc5405c7fcea958f683cb
Vojta Stavik <stavik@outlook.com>
2020-01-07 16:09:01 +0100

    [UIKit] Implement ViewController and its tests

--- WeatherApp-UIKit/WeatherApp-UIKit/ViewController.swift
+++ WeatherApp-UIKit/WeatherApp-UIKit/ViewController.swift
@@ -7,14 +7,89 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+  // MARK: Dependencies
+
+  var viewModel: WeatherViewModelType!
+
+  // MARK: Outlets
+
+  @IBOutlet weak var iconLabel: UILabel!
+  @IBOutlet weak var weatherDescriptionLabel: UILabel!
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+
+  @IBOutlet weak var dislikeButton: UIButton!
+  @IBOutlet weak var likeButton: UIButton!
+
+  @IBOutlet weak var contentView: UIStackView!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet weak var errorLabel: UILabel!
+
+  // MARK: Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    viewModel.state
+      .drive(onNext: { [unowned self] (state) in
+        switch state {
+        case .loading: self.loading()
+
+        case let .loaded(data):
+          self.loaded(
+            weatherDescription: data.weatherDescription,
+            temperature: data.temperature,
+            icon: data.icon,
+            location: data.location
+          )
+
+        case .error: self.error()
+        }
+      })
+      .disposed(by: disposeBag)
+
+    likeButton.rx.tap
+      .bind(to: viewModel.likeButtonTapped)
+      .disposed(by: disposeBag)
+
+    dislikeButton.rx.tap
+      .bind(to: viewModel.dislikeButtonTapped)
+      .disposed(by: disposeBag)
   }
 
-
+  private let disposeBag = DisposeBag()
 }
 
+extension ViewController {
+  private func loaded(weatherDescription: String, temperature: String, icon: String, location: String) {
+    update(visibility: .content)
+
+    iconLabel.text = icon
+    weatherDescriptionLabel.text = weatherDescription
+    temperatureLabel.text = temperature
+    locationLabel.text = location
+  }
+
+  private func loading() {
+    update(visibility: .loadingIndicator)
+  }
+
+  private func error(message: String? = nil) {
+    errorLabel.text = message ?? "🤷‍♂️\nSorry, something went wrong."
+    update(visibility: .error)
+  }
+
+  private enum Visibility {
+    case content, loadingIndicator, error
+  }
+
+  private func update(visibility: Visibility) {
+    contentView.isHidden = visibility != .content
+    activityIndicator.isHidden = visibility != .loadingIndicator
+    errorLabel.isHidden = visibility != .error
+  }
+}
---
+++ WeatherApp-UIKit/WeatherApp-UIKit/ViewControllerTests.swift
@@ -0,0 +1,92 @@
+//
+//  ViewControllerTests.swift
+//  WeatherApp-UIKitTests
+//
+//  Created by Vojta on 07/01/2020.
+//  Copyright © 2020 Industrial Binaries. All rights reserved.
+//
+
+@testable import WeatherApp_UIKit
+
+import UIKit
+import XCTest
+import RxTest
+import RxSwift
+import RxCocoa
+import SnapshotTesting
+
+struct MockViewModel: WeatherViewModelType {
+  // Outputs
+  var state: Driver<WeatherViewModelState> = .never()
+
+  // Inputs
+  var likeButtonTapped: PublishSubject<Void> = .init()
+  var dislikeButtonTapped: PublishSubject<Void> =  .init()
+}
+
+class ViewControllerTests: XCTestCase {
+
+  var sut: ViewController!
+
+  var scheduler: TestScheduler!
+
+  var disposeBag: DisposeBag!
+
+  override func setUp() {
+    super.setUp()
+
+    scheduler = .init(initialClock: 0)
+    disposeBag = .init()
+
+    sut = UIStoryboard(name: "Main", bundle: nil)
+      .instantiateViewController(identifier: "ViewController") as ViewController
+
+    XCTAssertEqual(UIScreen.main.scale, 2, "Snapshots are valid only for @2 scale")
+  }
+
+  func testLoadingData() {
+    let vm = MockViewModel(state: .just(.loading))
+    sut.viewModel = vm
+
+    sut.view.layer.speed = 0 // to stop the spinner
+
+    assertSnapshot(matching: sut, as: .image(on: .iPhoneX))
+  }
+
+  func testLoadedData() {
+    let vm = MockViewModel(state: .just(.loaded(
+      weatherDescription: "Amazingly sunny",
+      temperature: "112 °C",
+      icon: "🚀",
+      location: "Virtual"
+    )))
+    sut.viewModel = vm
+
+    assertSnapshot(matching: sut, as: .image(on: .iPhoneX))
+  }
+
+  func testLikeDislikeButtonsTap() {
+    let vm = MockViewModel()
+
+    sut.viewModel = vm
+    sut.loadViewIfNeeded()
+
+    let like = scheduler.createObserver(Void.self)
+    let dislike = scheduler.createObserver(Void.self)
+
+    vm.likeButtonTapped.bind(to: like).disposed(by: disposeBag)
+    vm.dislikeButtonTapped.bind(to: dislike).disposed(by: disposeBag)
+
+    sut.dislikeButton.sendActions(for: .touchUpInside)
+    sut.likeButton.sendActions(for: .touchUpInside)
+
+    XCTAssertEqual(like.events.count, 1)
+    XCTAssertEqual(dislike.events.count, 1)
+  }
+
+    func testErrorState() {
+    let vm = MockViewModel(state: .just(.error))
+    sut.viewModel = vm
+    assertSnapshot(matching: sut, as: .image(on: .iPhoneX))
+  }
+}
