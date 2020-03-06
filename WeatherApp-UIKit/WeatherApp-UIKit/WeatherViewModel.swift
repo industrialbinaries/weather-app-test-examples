bbad45b7895cb8ae839e577d93790eb0170b4c30
Jozef Matus <jojo@matus.sk>
2020-03-06 10:13:41 +0100

    Remove binding from view model

--- WeatherApp-UIKit/WeatherApp-UIKit/WeatherViewModel.swift
+++ WeatherApp-UIKit/WeatherApp-UIKit/WeatherViewModel.swift
@@ -53,75 +53,69 @@ extension WeatherViewModelState {
   }
 }
 
-protocol WeatherViewModelType {
-  // Outputs
-  var state: Driver<WeatherViewModelState> { get }
+typealias API = (CLLocationCoordinate2D) -> Observable<Weather>
+typealias Storage = (Weather, _ liked: Bool) -> Void
+typealias StateMachineViewModelInput = (
+  likeButtonTapped: Observable<Void>,
+  dislikeButtonTapped: Observable<Void>
+)
+typealias StateMachineViewModelOutput = Driver<WeatherViewModelState>
+typealias StateMachineViewModel = (
+  @escaping API,
+  @escaping Storage,
+  Locale,
+  Observable<LocationProvider.State>,
+  StateMachineViewModelInput
+  ) -> StateMachineViewModelOutput
 
-  // Inputs
-  var likeButtonTapped: PublishSubject<Void> { get }
-  var dislikeButtonTapped: PublishSubject<Void> { get }
-}
-
-struct WeatherViewModel: WeatherViewModelType {
-  typealias API = (CLLocationCoordinate2D) -> Observable<Weather>
-  typealias Storage = (Weather, _ liked: Bool) -> Void
-
-  // Outputs
-  let state: Driver<WeatherViewModelState>
-
-  // Inputs
-  let likeButtonTapped: PublishSubject<Void> = .init()
-  let dislikeButtonTapped: PublishSubject<Void> = .init()
-
-  init(
-    weatherAPI: @escaping API = WeatherAPI.loadWeatherData,
-    storage: @escaping Storage = { _, _ in },
-    locale: Locale = .current,
-    currentLocation: Observable<LocationProvider.State>
-  ) {
-    let currentLocation = currentLocation.share()
-
-    let weather: Observable<Weather> = currentLocation
-      .flatMap { location -> Observable<Weather> in
-        switch location {
-        case .loading, .error:
-          return Observable.never()
-
-        case let .location(location):
-          return weatherAPI(location)
-        }
+func stateMachineViewModel(
+  weatherAPI: @escaping API,
+  storage: @escaping Storage,
+  locale: Locale,
+  currentLocation: Observable<LocationProvider.State>,
+  input: StateMachineViewModelInput
+) -> StateMachineViewModelOutput {
+  let currentLocation = currentLocation.share()
+  
+  let weather: Observable<Weather> = currentLocation
+    .flatMap { location -> Observable<Weather> in
+      switch location {
+      case .loading, .error:
+        return Observable.never()
+        
+      case let .location(location):
+        return weatherAPI(location)
       }
-      .share()
-
-    likeButtonTapped
-      .withLatestFrom(weather)
-      .map { ($0, liked: true) }
-      .subscribe(onNext: storage)
-      .disposed(by: disposeBag)
-
-    dislikeButtonTapped
-      .withLatestFrom(weather)
-      .map { ($0, liked: false) }
-      .subscribe(onNext: storage)
-      .disposed(by: disposeBag)
-
-    let loadingAndError: Observable<WeatherViewModelState> = currentLocation
-      .map { location in
-        switch location {
-        case .error: return .error
-
-        case .location, .loading:
-          // `.location` also emits `loading` as a plaholder value until the API
-          // call has finished
-          return .loading
-        }
-      }
-
-    state = Observable.merge(
-      loadingAndError,
-      weather.map { WeatherViewModelState(weather: $0, locale: locale) }
-    ).asDriver(onErrorJustReturn: .error)
   }
-
-  private let disposeBag = DisposeBag()
+  .share()
+  
+  let loadingAndError: Observable<WeatherViewModelState> = currentLocation
+    .map { location in
+      switch location {
+      case .error: return .error
+        
+      case .location, .loading:
+        return .loading
+      }
+  }
+  
+  let like = input.likeButtonTapped.withLatestFrom(weather).map { weather -> WeatherViewModelState in
+    storage(weather, true)
+    return WeatherViewModelState(weather: weather, locale: locale, weatherFeedback: true)
+  }
+  
+  let dislike = input.dislikeButtonTapped.withLatestFrom(weather).map { weather -> WeatherViewModelState in
+    storage(weather, false)
+    return WeatherViewModelState(weather: weather, locale: locale, weatherFeedback: true)
+  }
+  
+  let state = Observable.merge(
+    loadingAndError,
+    like,
+    dislike,
+    weather.map { WeatherViewModelState(weather: $0, locale: locale) }
+  )
+    .asDriver(onErrorJustReturn: .error)
+  
+  return state
 }
