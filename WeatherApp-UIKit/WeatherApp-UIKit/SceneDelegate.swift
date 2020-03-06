0ec2775cd4ec64dbd21b8b6620c419196a5fbffe
Jozef Matus <jojo@matus.sk>
2020-03-06 10:14:23 +0100

    Update creation of view controller and view model

--- WeatherApp-UIKit/WeatherApp-UIKit/SceneDelegate.swift
+++ WeatherApp-UIKit/WeatherApp-UIKit/SceneDelegate.swift
@@ -5,27 +5,34 @@
 //  MIT license, see LICENSE file for details
 //
 
+import RxSwift
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-
-  func scene(
-    _ scene: UIScene,
-    willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    guard let windowScene = scene as? UIWindowScene else { return }
-
-    let window = UIWindow(windowScene: windowScene)
-
-    let rootVC = UIStoryboard(name: "Main", bundle: nil)
-      .instantiateInitialViewController() as! ViewController
-    rootVC.viewModel = WeatherViewModel(currentLocation: LocationProvider.shared.currentLocation)
-
-    window.rootViewController = rootVC
-    window.makeKeyAndVisible()
-
-    self.window = window
-  }
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        let rootVC = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateInitialViewController() as! ViewController
+        
+        let input: StateMachineViewModelInput = (rootVC.likeButton.rx.tap.asObservable(), rootVC.dislikeButton.rx.tap.asObservable())
+        rootVC.viewModel = stateMachineViewModel(weatherAPI: WeatherAPI.loadWeatherData,
+                                                 storage: { _, _ in print("saved") },
+                                                 locale: .current,
+                                                 currentLocation: LocationProvider.shared.currentLocation,
+                                                 input: input)
+        
+        window.rootViewController = rootVC
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
 }
