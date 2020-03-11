06bc6973ea166f3d5f0d10aea7814117cbcf2ec4
Jozef Matus <jojo@matus.sk>
2020-03-11 15:27:31 +0100

    Implement new ViewModel without subscriptions

--- WeatherApp-UIKit/WeatherApp-UIKit/WeatherViewModel.swift
+++ WeatherApp-UIKit/WeatherApp-UIKit/WeatherViewModel.swift
@@ -97,7 +97,7 @@ typealias StateMachineViewModel = (
   Locale,
   Observable<LocationProvider.State>,
   StateMachineViewModelInput
-  ) -> StateMachineViewModelOutput
+) -> StateMachineViewModelOutput
 
 func stateMachineViewModel(
   weatherAPI: @escaping WeatherViewModel.API,
@@ -106,6 +106,47 @@ func stateMachineViewModel(
   currentLocation: Observable<LocationProvider.State>,
   input: StateMachineViewModelInput
 ) -> StateMachineViewModelOutput {
-  return Driver.just(.loading)
-}
+  let currentLocation = currentLocation.share()
 
+  let weather: Observable<Weather> = currentLocation
+    .flatMap { location -> Observable<Weather> in
+      switch location {
+      case .loading, .error:
+        return Observable.never()
+
+      case let .location(location):
+        return weatherAPI(location)
+      }
+    }
+    .share()
+
+  let loadingAndError: Observable<WeatherViewModelState> = currentLocation
+    .map { location in
+      switch location {
+      case .error: return .error
+
+      case .location, .loading:
+        return .loading
+      }
+    }
+
+  let like = input.likeButtonTapped.asObservable().withLatestFrom(weather).map { weather -> WeatherViewModelState in
+    storage(weather, true)
+    return WeatherViewModelState(weather: weather, locale: locale, weatherFeedback: .like)
+  }
+
+  let dislike = input.dislikeButtonTapped.asObservable().withLatestFrom(weather).map { weather -> WeatherViewModelState in
+    storage(weather, false)
+    return WeatherViewModelState(weather: weather, locale: locale, weatherFeedback: .dislike)
+  }
+
+  let state = Observable.merge(
+    loadingAndError,
+    like,
+    dislike,
+    weather.map { WeatherViewModelState(weather: $0, locale: locale) }
+  )
+  .asDriver(onErrorJustReturn: .error)
+
+  return state
+}
